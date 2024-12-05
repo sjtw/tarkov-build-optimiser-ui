6b98934148eea7ac56c3e1e79ff813b4a0279274
sjtw <simonjtweed@gmail.com>
2024-12-05 04:52:07 +0000

    fix build failure

--- app/optimiser/page.tsx
+++ app/optimiser/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Metadata } from "next";
 import Filters from "@/app/ui/filters/filters";
 import { getWeaponDefaultPresets } from "@/app/lib/tarkov-api";
@@ -11,7 +11,9 @@ async function Page() {
   const presets = await getWeaponDefaultPresets();
   return (
     <div className="flex pb-0 h-full">
-      <Filters presets={presets} />
+      <Suspense>
+        <Filters presets={presets} />
+      </Suspense>
       <div className="flex-grow overflow-y-scroll p-4">
         <p className="text-center w-full justify-center">
           Weapon build will be displayed once a weapon & trader levels are
